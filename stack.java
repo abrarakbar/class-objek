b979905330275de4e43bfd7ffb6fea3f40840c36
abrarakbar <43749992+abrarakbar@users.noreply.github.com>
2018-10-10 01:23:52 +0800

    Create stack.java

---
+++ stack.java
@@ -0,0 +1,13 @@
+
+public class stack {
+	
+	//Attributtes
+			String[] value = new String[100];
+			int top;
+			
+			//Method setter
+				String setvalueAt(int position, String value){
+					return value;
+			}
+
+}
